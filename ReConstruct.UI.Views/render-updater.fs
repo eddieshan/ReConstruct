e5adeed0ddb7d11d0d992d824226f891b355b8ea
edshan <edshan@none.none>
2020-03-02 22:21:36 +0100

    Refactor render updater to separate file.

---
+++ ReConstruct.UI.Views/render-updater.fs
@@ -0,0 +1,36 @@
+﻿namespace ReConstruct.UI.View
+
+open System
+
+// An event based status updater that detaches itself when render is done.
+module Render =
+
+    let updater() =
+
+        let updateRenderStatus = new Event<int*int*float>()
+        let mutable polygonizedCount = 1
+        let mutable finished = false
+        let mutable totalTriangles = 0
+
+        let handleUpdate (slicesCount, numberOfTriangles, time) =
+            polygonizedCount <- polygonizedCount + 1
+            totalTriangles <- totalTriangles + numberOfTriangles
+            let percentage = (float polygonizedCount)*100.0/(float slicesCount)
+            finished <- polygonizedCount = slicesCount
+            match finished with
+            | true  -> Events.Status.Trigger(sprintf "%fs | %i triangles" time totalTriangles)
+            | false -> Events.Status.Trigger(sprintf "Completed %i of %i : %.0f%% | %i triangles" polygonizedCount slicesCount percentage totalTriangles)
+
+        let start() =
+            Events.Status.Trigger("Rendering volume, please wait ...")
+            updateRenderStatus.Publish.Subscribe handleUpdate
+
+        let subscription = start()
+        
+        let updateOrStop (count, numberOfTriangles, time) = 
+            if finished then
+                subscription.Dispose()
+            else
+                updateRenderStatus.Trigger (count, numberOfTriangles, time)
+
+        updateOrStop
--- ReConstruct.UI.Views/volume-view.fs
+++ ReConstruct.UI.Views/volume-view.fs
@@ -20,40 +20,6 @@ module private RenderAgent =
     let private throttledForkJoinAgent = Async.throttlingAgent parallelThrottle context
     let enqueueJob = ThrottledJob >> throttledForkJoinAgent.Post
 
-// An event based status updater that detaches itself when render is done.
-module Render =
-
-    let updater() =
-
-        let updateRenderStatus = new Event<int*int*float>()
-        let mutable polygonizedCount = 1
-        let mutable finished = false
-        let mutable totalTriangles = 0
-
-        let handleUpdate (slicesCount, numberOfTriangles, time) =
-            polygonizedCount <- polygonizedCount + 1
-            totalTriangles <- totalTriangles + numberOfTriangles
-            let percentage = (float polygonizedCount)*100.0/(float slicesCount)
-            finished <- polygonizedCount = slicesCount
-            match finished with
-            | true  -> Events.Status.Trigger(sprintf "%fs | %i triangles" time totalTriangles)
-            | false -> Events.Status.Trigger(sprintf "Completed %i of %i : %.0f%% | %i triangles" polygonizedCount slicesCount percentage totalTriangles)
-
-        let start() =
-            Events.Status.Trigger("Rendering volume, please wait ...")
-            updateRenderStatus.Publish.Subscribe handleUpdate
-
-        let subscription = start()
-        
-        let updateOrStop (count, numberOfTriangles, time) = 
-            if finished then
-                subscription.Dispose()
-            else
-                updateRenderStatus.Trigger (count, numberOfTriangles, time)
-
-        updateOrStop
-
-
 // Slice series containing Hounsfield buffers are projected into section volumes using the marching cubes algorithm.
 // The scene is set up based on the total volume size and position.
 module VolumeView = 
