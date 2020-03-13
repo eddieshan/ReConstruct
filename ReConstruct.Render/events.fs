d544fffc74efa3d358d1969005eb432fb2de6c32
eddieshan <6906040+eddieshan@users.noreply.github.com>
2020-03-14 00:37:02 +0100

    New Render project for all platform independent, rendering related code.

---
+++ ReConstruct.Render/events.fs
@@ -0,0 +1,12 @@
+﻿namespace ReConstruct.Render
+
+type Axis =
+| X
+| Y
+| Z
+
+module Events =
+    let OnRotation = new Event<Axis*float32>()
+    let OnCameraMoved = new Event<float32>()
+    let OnScale = new Event<float32>()
+    let RenderStatus = new Event<string>()
