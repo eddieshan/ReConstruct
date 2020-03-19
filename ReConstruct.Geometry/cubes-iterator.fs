b117188266b1b8ce46bfafda996cc43ce2dff454
eddieshan <6906040+eddieshan@users.noreply.github.com>
2020-03-19 23:43:51 +0100

    Remove redundant sums and simply traversal algorithm.

--- ReConstruct.Geometry/cubes-iterator.fs
+++ ReConstruct.Geometry/cubes-iterator.fs
@@ -16,13 +16,6 @@ module CubesIterator =
         }
 
     let iterate (front, back) isoValue addPoints polygonize = 
-        let cube = 
-            {
-                IsoValue = isoValue
-                Vertices = Array.zeroCreate<Vector3> 8
-                Levels = Array.zeroCreate<int> 8
-                AddPoint = addPoints
-             }
 
         let setValues (row, column) x =
             let rowPlus1 = row + 1
@@ -40,50 +33,39 @@ module CubesIterator =
         let zFront, zBack = float32 front.SliceParams.UpperLeft.[2], float32 back.SliceParams.UpperLeft.[2]
         let stepX, stepY = float32 front.SliceParams.PixelSpacing.X, float32 front.SliceParams.PixelSpacing.Y
 
-        let yTopFront = float32 front.SliceParams.UpperLeft.[1]
-        let yBottomFront = yTopFront + stepY
+        let top, left = float32 front.SliceParams.UpperLeft.[1], float32 front.SliceParams.UpperLeft.[0]
+        let bottom, right = top + stepY, left + stepX
 
-        let yTopBack = float32 back.SliceParams.UpperLeft.[1]
-        let yBottomBack = yTopBack + float32 back.SliceParams.PixelSpacing.Y
-
-        cube.Vertices.[0].Z <- zBack
-        cube.Vertices.[1].Z <- zBack
-        cube.Vertices.[2].Z <- zFront
-        cube.Vertices.[3].Z <- zFront
-        cube.Vertices.[4].Z <- zBack
-        cube.Vertices.[5].Z <- zBack
-        cube.Vertices.[6].Z <- zFront
-        cube.Vertices.[7].Z <- zFront
-
-        cube.Vertices.[0].Y <- yBottomBack
-        cube.Vertices.[1].Y <- yBottomBack
-        cube.Vertices.[2].Y <- yBottomFront
-        cube.Vertices.[3].Y <- yBottomFront
-        cube.Vertices.[4].Y <- yTopBack
-        cube.Vertices.[5].Y <- yTopBack
-        cube.Vertices.[6].Y <- yTopFront
-        cube.Vertices.[7].Y <- yTopFront
+        let cube = 
+            {
+                IsoValue = isoValue
+                Vertices = [|
+                    Vector3(0.0f, bottom, zBack)
+                    Vector3(0.0f, bottom, zBack)
+                    Vector3(0.0f, bottom, zFront)
+                    Vector3(0.0f, bottom, zFront)
+                    Vector3(0.0f, top, zBack)
+                    Vector3(0.0f, top, zBack)
+                    Vector3(0.0f, top, zFront)
+                    Vector3(0.0f, top, zFront)
+                |]
+                Levels = Array.zeroCreate<int> 8
+                AddPoint = addPoints
+             }
 
         for row in 0..front.SliceParams.Dimensions.Rows - 2 do
-            let xLeftFront = float32 front.SliceParams.UpperLeft.[0]
-            let xRightFront = xLeftFront + stepX
-
-            let xLeftBack = float32 back.SliceParams.UpperLeft.[0]
-            let xRightBack = xLeftBack + stepX
-
-            cube.Vertices.[0].X <- xLeftBack
-            cube.Vertices.[1].X <- xRightBack
-            cube.Vertices.[2].X <- xRightFront
-            cube.Vertices.[3].X <- xLeftFront
-            cube.Vertices.[4].X <- xLeftBack
-            cube.Vertices.[5].X <- xRightBack
-            cube.Vertices.[6].X <- xRightFront
-            cube.Vertices.[7].X <- xLeftFront
+            cube.Vertices.[0].X <- left
+            cube.Vertices.[1].X <- right
+            cube.Vertices.[2].X <- right
+            cube.Vertices.[3].X <- left
+            cube.Vertices.[4].X <- left
+            cube.Vertices.[5].X <- right
+            cube.Vertices.[6].X <- right
+            cube.Vertices.[7].X <- left
 
             for column in 0..front.SliceParams.Dimensions.Columns - 2 do
 
                 setValues (row, column) cube
-
                 polygonize cube
 
                 for n in 0..7 do
