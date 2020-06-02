df0c1a1ef105873bd7e355506d9106501eb7a2fe
eddieshan <6906040+eddieshan@users.noreply.github.com>
2020-06-02 18:36:10 +0200

    Gradient approximation using kernel smoothing.

--- ReConstruct.Geometry/cubes-gradient-iterator.fs
+++ ReConstruct.Geometry/cubes-gradient-iterator.fs
@@ -19,7 +19,10 @@ module CubesGradientIterator =
 
         let vertices = Array.zeroCreate<Vector3> 12
         let gradients = Array.zeroCreate<Vector3> 12
-        let front, back, next = slices.[frontIndex].HField, slices.[frontIndex + 1].HField, slices.[frontIndex + 2].HField
+        let front, back = slices.[frontIndex].HField, slices.[frontIndex + 1].HField
+
+        let backIndex = frontIndex + 1
+        let gradient = Gradient(slices)
 
         let processCube tLeft row column =
             let tRight, bLeft = tLeft + jumpColumn, tLeft + jumpRow
@@ -37,49 +40,14 @@ module CubesGradientIterator =
             let cubeIndex = cube.GetIndex()
 
             if EdgeTable.[cubeIndex] <> 0 then
-
-                let jumpUnder = (1 - row/lastRow) * jumpRow
-                let uBLeft, uBRight = bLeft + jumpUnder, bRight + jumpUnder
-
-                let jumpNextRight = (1 - column/lastColumn) * jumpColumn
-                let rBRight, rTRight = bRight + jumpNextRight, tRight + jumpNextRight
-
-                let backBLeft, backBRight = 2s*back.[bLeft], 2s*back.[bRight]
-                let frontBRight, frontBLeft = 2s*front.[bRight], 2s*front.[bLeft]
-                let backTLeft, backTRight = 2s*back.[tLeft], 2s*back.[tRight]
-                let frontTRight, frontTLeft = 2s*front.[tRight], 2s*front.[tLeft]
-
-                cube.Gradients.[0].X <- ((next.[bRight] + back.[bRight] - backBLeft) |> float32)*0.5f
-                cube.Gradients.[0].Y <- ((next.[uBLeft] + back.[uBLeft] - backBLeft) |> float32)*0.5f
-                cube.Gradients.[0].Z <- (next.[bLeft] - back.[bLeft]) |> float32
-
-                cube.Gradients.[1].X <- ((next.[rBRight] + back.[rBRight] - backBRight) |> float32)*0.5f
-                cube.Gradients.[1].Y <- ((next.[uBRight] + back.[uBRight] - backBRight) |> float32)*0.5f
-                cube.Gradients.[1].Z <- (next.[bRight] - back.[bRight]) |> float32
-
-                cube.Gradients.[2].X <- ((back.[rBRight] + front.[rBRight] - frontBRight) |> float32)*0.5f
-                cube.Gradients.[2].Y <- ((back.[uBRight] + front.[uBRight] - frontBRight) |> float32)*0.5f
-                cube.Gradients.[2].Z <- (back.[bRight] - front.[bRight]) |> float32
-
-                cube.Gradients.[3].X <- ((back.[bRight] + front.[bRight] - frontBLeft) |> float32)*0.5f
-                cube.Gradients.[3].Y <- ((back.[uBLeft] + front.[uBLeft] - frontBLeft) |> float32)*0.5f
-                cube.Gradients.[3].Z <- (back.[bLeft] - front.[bLeft]) |> float32
-
-                cube.Gradients.[4].X <- ((next.[tRight] + back.[tRight] - backTLeft) |> float32)*0.5f
-                cube.Gradients.[4].Y <- ((next.[bLeft] + back.[bLeft] - backTLeft) |> float32)*0.5f
-                cube.Gradients.[4].Z <- (next.[tLeft] - back.[tLeft]) |> float32
-
-                cube.Gradients.[5].X <- ((next.[rTRight] + back.[rTRight] - backTRight) |> float32)*0.5f
-                cube.Gradients.[5].Y <- ((next.[bRight] + back.[bRight] - backTRight) |> float32)*0.5f
-                cube.Gradients.[5].Z <- (next.[tRight] - back.[tRight]) |> float32
-
-                cube.Gradients.[6].X <- ((back.[rTRight] + front.[rTRight] - frontTRight) |> float32)*0.5f
-                cube.Gradients.[6].Y <- ((back.[bRight] + front.[bRight] - frontTRight) |> float32)*0.5f
-                cube.Gradients.[6].Z <- (back.[tRight] - front.[tRight]) |> float32
-
-                cube.Gradients.[7].X <- ((back.[tRight] + front.[tRight] - frontTLeft) |> float32)*0.5f
-                cube.Gradients.[7].Y <- ((back.[bLeft] + front.[bLeft] - frontTLeft) |> float32)*0.5f
-                cube.Gradients.[7].Z <- (back.[tLeft] - front.[tLeft]) |> float32
+                gradient.setValue (backIndex, bLeft, &cube.Gradients.[0])
+                gradient.setValue (backIndex, bRight, &cube.Gradients.[1])
+                gradient.setValue (frontIndex, bRight, &cube.Gradients.[2])
+                gradient.setValue (frontIndex, bLeft, &cube.Gradients.[3])
+                gradient.setValue (backIndex, tLeft, &cube.Gradients.[4])
+                gradient.setValue (backIndex, tRight, &cube.Gradients.[5])
+                gradient.setValue (frontIndex, tRight, &cube.Gradients.[6])
+                gradient.setValue (frontIndex, tLeft, &cube.Gradients.[7])
         
                 for i in 0..EdgeTraversal.Length-1 do
                     if (EdgeTable.[cubeIndex] &&& (1 <<< i)) > 0 then
---
+++ ReConstruct.Geometry/gradient.fs
@@ -0,0 +1,56 @@
+﻿namespace ReConstruct.Geometry
+
+open System
+open System.Numerics
+
+open ReConstruct.Data.Dicom
+
+module Gradient =
+
+    let d1 = 2.0 |> Math.Sqrt |> float32
+    let d2 = 3.0 |> Math.Sqrt |> float32
+    let inline avg (v0, v1, v2) = ((float32 v0) + (float32 v1)/d1 + (float32 v2)/d2)/18.0f
+
+type Gradient(slices: ImageSlice[]) =
+
+    // Approximate gradient with a smooth kernel based on averaging weighted partial derivatives.
+    // Weights are the inverse of the distance to the point.
+    member this.setValue (index, pos, g: byref<Vector3>) = 
+        let numRows = slices.[index].Columns
+        let prev, current, next = slices.[index - 1].HField, slices.[index].HField, slices.[index + 1].HField
+        let right, left = pos + 1, pos - 1
+        let under, above = pos + numRows, pos - numRows
+        let aRight, aLeft = above + 1, above - 1
+        let uRight, uLeft = under + 1, under - 1
+
+        g.X <- 
+            Gradient.avg(current.[right] - current.[pos] - current.[left] + current.[pos],
+                current.[aRight] - current.[pos] - current.[aLeft] + current.[pos] + 
+                current.[uRight] - current.[pos] - current.[uLeft] + current.[pos] + 
+                prev.[right] - current.[pos] - prev.[left] + current.[pos] +
+                next.[right] - current.[pos] - next.[left] + current.[pos],
+                prev.[aRight] - current.[pos] - prev.[aLeft] + current.[pos] + 
+                prev.[uRight] - current.[pos] - prev.[uLeft] + current.[pos] + 
+                next.[aRight] - current.[pos] - next.[aLeft] + current.[pos] + 
+                next.[uRight] - current.[pos] - next.[uLeft] + current.[pos])
+        g.Y <- 
+            Gradient.avg(current.[under] - current.[pos] - current.[above] + current.[pos],
+                current.[uRight] - current.[pos] - current.[aRight] + current.[pos] +
+                current.[uLeft] - current.[pos] - current.[aLeft] + current.[pos] +
+                prev.[under] - current.[pos] - prev.[above] + current.[pos] +
+                next.[under] - current.[pos] - next.[above] + current.[pos],
+                prev.[uRight] - current.[pos] - prev.[aRight] + current.[pos] +
+                prev.[uLeft] - current.[pos] - prev.[aLeft] + current.[pos] +
+                next.[uRight] - current.[pos] - next.[aRight] + current.[pos] +
+                next.[uLeft] - current.[pos] - next.[aLeft] + current.[pos])
+        g.Z <- 
+            Gradient.avg(next.[pos] - current.[pos] - prev.[pos] + current.[pos],
+                next.[right] - current.[pos] - prev.[right] + current.[pos] +
+                next.[left] - current.[pos] - prev.[left] + current.[pos] +
+                next.[under] - current.[pos] - prev.[under] + current.[pos] +
+                next.[above] - current.[pos] - prev.[above] + current.[pos],
+                next.[aLeft] - current.[pos] - prev.[aLeft] + current.[pos] +
+                next.[uLeft] - current.[pos] - prev.[uLeft] + current.[pos] +
+                next.[aRight] - current.[pos] - prev.[aRight] + current.[pos] +
+                next.[uRight] - current.[pos] - prev.[uRight] + current.[pos])
+
