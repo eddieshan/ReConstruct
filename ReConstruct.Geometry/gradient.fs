9b5ad45b52cd2efa6a013af6f95769a3d8e53499
eddieshan <6906040+eddieshan@users.noreply.github.com>
2020-06-02 19:39:09 +0200

    Refactor and clean up.

--- ReConstruct.Geometry/gradient.fs
+++ ReConstruct.Geometry/gradient.fs
@@ -5,16 +5,20 @@ open System.Numerics
 
 open ReConstruct.Data.Dicom
 
-module Gradient =
+module private Gradient =
 
     let d1 = 2.0 |> Math.Sqrt |> float32
     let d2 = 3.0 |> Math.Sqrt |> float32
     let inline avg (v0, v1, v2) = ((float32 v0) + (float32 v1)/d1 + (float32 v2)/d2)/18.0f
 
+// Approximate gradient with a smooth kernel based on averaging weighted partial derivatives taking neighbour points.
+// Weights are the inverse of the distance from the gradient point to its neighbours.
+// Distances have 3 possible values,
+// 1       -> Neighbour adjacent on the same axis.
+// Sqrt(2) -> Neighbour on the opposite side of a coplanar diagonal.
+// Sqrt(3) -> Neighbour on the opposite side of a non coplanar diagonal.
 type Gradient(slices: ImageSlice[]) =
 
-    // Approximate gradient with a smooth kernel based on averaging weighted partial derivatives.
-    // Weights are the inverse of the distance to the point.
     member this.setValue (index, pos, g: byref<Vector3>) = 
         let numRows = slices.[index].Columns
         let prev, current, next = slices.[index - 1].HField, slices.[index].HField, slices.[index + 1].HField
@@ -24,33 +28,33 @@ type Gradient(slices: ImageSlice[]) =
         let uRight, uLeft = under + 1, under - 1
 
         g.X <- 
-            Gradient.avg(current.[right] - current.[pos] - current.[left] + current.[pos],
-                current.[aRight] - current.[pos] - current.[aLeft] + current.[pos] + 
-                current.[uRight] - current.[pos] - current.[uLeft] + current.[pos] + 
-                prev.[right] - current.[pos] - prev.[left] + current.[pos] +
-                next.[right] - current.[pos] - next.[left] + current.[pos],
-                prev.[aRight] - current.[pos] - prev.[aLeft] + current.[pos] + 
-                prev.[uRight] - current.[pos] - prev.[uLeft] + current.[pos] + 
-                next.[aRight] - current.[pos] - next.[aLeft] + current.[pos] + 
-                next.[uRight] - current.[pos] - next.[uLeft] + current.[pos])
+            Gradient.avg(current.[right] - current.[left],
+                current.[aRight] - current.[aLeft] + 
+                current.[uRight] - current.[uLeft] + 
+                prev.[right] - prev.[left] +
+                next.[right] - next.[left],
+                prev.[aRight] - prev.[aLeft] + 
+                prev.[uRight] - prev.[uLeft] + 
+                next.[aRight] - next.[aLeft] + 
+                next.[uRight] - next.[uLeft])
         g.Y <- 
-            Gradient.avg(current.[under] - current.[pos] - current.[above] + current.[pos],
-                current.[uRight] - current.[pos] - current.[aRight] + current.[pos] +
-                current.[uLeft] - current.[pos] - current.[aLeft] + current.[pos] +
-                prev.[under] - current.[pos] - prev.[above] + current.[pos] +
-                next.[under] - current.[pos] - next.[above] + current.[pos],
-                prev.[uRight] - current.[pos] - prev.[aRight] + current.[pos] +
-                prev.[uLeft] - current.[pos] - prev.[aLeft] + current.[pos] +
-                next.[uRight] - current.[pos] - next.[aRight] + current.[pos] +
-                next.[uLeft] - current.[pos] - next.[aLeft] + current.[pos])
+            Gradient.avg(current.[under] - current.[above],
+                current.[uRight] - current.[aRight] +
+                current.[uLeft] - current.[aLeft] +
+                prev.[under] - prev.[above] +
+                next.[under] - next.[above],
+                prev.[uRight] - prev.[aRight] +
+                prev.[uLeft] - prev.[aLeft] +
+                next.[uRight] - next.[aRight] +
+                next.[uLeft] - next.[aLeft])
         g.Z <- 
-            Gradient.avg(next.[pos] - current.[pos] - prev.[pos] + current.[pos],
-                next.[right] - current.[pos] - prev.[right] + current.[pos] +
-                next.[left] - current.[pos] - prev.[left] + current.[pos] +
-                next.[under] - current.[pos] - prev.[under] + current.[pos] +
-                next.[above] - current.[pos] - prev.[above] + current.[pos],
-                next.[aLeft] - current.[pos] - prev.[aLeft] + current.[pos] +
-                next.[uLeft] - current.[pos] - prev.[uLeft] + current.[pos] +
-                next.[aRight] - current.[pos] - prev.[aRight] + current.[pos] +
-                next.[uRight] - current.[pos] - prev.[uRight] + current.[pos])
+            Gradient.avg(next.[pos] - prev.[pos],
+                next.[right] - prev.[right] +
+                next.[left] - prev.[left] +
+                next.[under] - prev.[under] +
+                next.[above] - prev.[above],
+                next.[aLeft] - prev.[aLeft] +
+                next.[uLeft] - prev.[uLeft] +
+                next.[aRight] - prev.[aRight] +
+                next.[uRight] - prev.[uRight])
 
