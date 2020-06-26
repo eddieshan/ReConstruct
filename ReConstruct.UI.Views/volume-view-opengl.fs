f8581bf9ba9b949ff36bfde47bb7bc1956c9a35f
eddieshan <6906040+eddieshan@users.noreply.github.com>
2020-06-26 20:49:32 +0200

    Refactor volume centroid calculation.

--- ReConstruct.UI.Views/volume-view-opengl.fs
+++ ReConstruct.UI.Views/volume-view-opengl.fs
@@ -11,25 +11,23 @@ open ReConstruct.Render.OpenGL
 
 module VolumeViewOpenGL = 
 
-    let getVolumeCenter firstSlice lastSlice =
-        let offset = Vector3(firstSlice.PixelSpacing.X * (float32 firstSlice.Columns) / 2.0f,
-                            firstSlice.PixelSpacing.Y * (float32 firstSlice.Rows) / 2.0f,
-                            Math.Abs(lastSlice.TopLeft.Z - firstSlice.TopLeft.Z) / 2.0f)
-        firstSlice.TopLeft + offset
+    let getVolumeCenter slice depth =
+        let offset = Vector3(slice.PixelSpacing.X*(float32 slice.Columns), slice.PixelSpacing.Y*(float32 slice.Rows), depth)      
+        slice.TopLeft + offset*Vector3(0.5f)
 
     // Build Volume View from sequence of Slices. 
     let New isoLevel slices = 
         let firstSlice, lastSlice = slices |> Array.head, slices |> Array.last
 
         // Volume center is the centroid of the paralelogram defined between the first and last slice.
-        let centroid = getVolumeCenter firstSlice lastSlice
-        let centeredSlices = slices |> Array.map(ImageSlice.adjustToCenter centroid)
-        let estimatedModelSize = Math.Abs(lastSlice.TopLeft.Z - firstSlice.TopLeft.Z) |> float32
+        let depth = Math.Abs(lastSlice.TopLeft.Z - firstSlice.TopLeft.Z)
+        let centroid = getVolumeCenter firstSlice depth        
+        let centeredSlices = slices |> Array.map(ImageSlice.adjustToCenter centroid)        
 
         let calculateMesh partialRender =
-            DualContouring.polygonize isoLevel centeredSlices partialRender
+            MarchingCubesExtended.polygonize isoLevel centeredSlices partialRender
         
         let clock = Stopwatch.StartNew()
         let onUpdate numPoints =
             sprintf "%.2fs | %i triangles" clock.Elapsed.TotalSeconds (numPoints/3) |> Events.Status.Trigger
-        RenderView.buildScene (estimatedModelSize, calculateMesh, onUpdate)
+        RenderView.buildScene (depth, calculateMesh, onUpdate)
