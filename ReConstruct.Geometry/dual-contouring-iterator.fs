e66d33aa4bac466bc2d5a12ed7b871ad6dc3ec10
eddieshan <6906040+eddieshan@users.noreply.github.com>
2020-07-02 20:52:39 +0200

    Calculate front top left of the dual cube only for contributing cubes.

--- ReConstruct.Geometry/dual-contouring-iterator.fs
+++ ReConstruct.Geometry/dual-contouring-iterator.fs
@@ -25,31 +25,40 @@ module DualContouringIterator =
 
         let jumpColumn, jumpRow = 1, volume.Slices.[frontIndex].Columns
         let gradient = Gradient(volume.Slices)
+
+        let values = Array.zeroCreate<int16> 8
+
+        let inline getIndex() =
+            let mutable cubeIndex = 0
+            for i in 0..values.Length-1 do
+                if (values.[i] <= volume.IsoValue) then
+                    cubeIndex <- cubeIndex ||| (1 <<< i)
+            cubeIndex
        
-        let findInnerVertex (frontTopLeft: Vector3) tLeft cube (section: int[]) = 
+        let findInnerVertex row column tLeft (section: int[]) = 
             let front, back = volume.Slices.[section.[0]].HField, volume.Slices.[section.[1]].HField
             let tRight, bLeft = tLeft + jumpColumn, tLeft + jumpRow
             let bRight = bLeft + jumpColumn
 
-            cube.Values.[0] <- back.[bLeft]
-            cube.Values.[1] <- back.[bRight]
-            cube.Values.[2] <- front.[bRight]
-            cube.Values.[3] <- front.[bLeft]
-            cube.Values.[4] <- back.[tLeft]
-            cube.Values.[5] <- back.[tRight]
-            cube.Values.[6] <- front.[tRight]
-            cube.Values.[7] <- front.[tLeft]
+            values.[0] <- back.[bLeft]
+            values.[1] <- back.[bRight]
+            values.[2] <- front.[bRight]
+            values.[3] <- front.[bLeft]
+            values.[4] <- back.[tLeft]
+            values.[5] <- back.[tRight]
+            values.[6] <- front.[tRight]
+            values.[7] <- front.[tLeft]
 
             let mutable bestFitVertex = Vector3.Zero
             let mutable bestFitGradient = Vector3.Zero
 
-            let cubeIndex = cube.GetIndex()
+            let cubeIndex = getIndex()
 
             let contributions = EdgeContributions.[cubeIndex]
 
             for n in contributions do
                 let indexA, indexB = int EdgeTraversal.[n].[0], int EdgeTraversal.[n].[1]
-                let v1, v2 = cube.Values.[indexA], cube.Values.[indexB]
+                let v1, v2 = values.[indexA], values.[indexB]
                 let delta = v2 - v1
 
                 let mu =
@@ -64,7 +73,12 @@ module DualContouringIterator =
                 bestFitVertex <- bestFitVertex + Vector3.Lerp(volume.VertexOffsets.[indexA], volume.VertexOffsets.[indexB], mu)
                 bestFitGradient <- bestFitGradient + Vector3.Lerp(gradientA, gradientB, mu)
 
+            // Cube front top left calculated only for contributing cubes. A more expensive calculation, done fewer times.
+            // Assuming that in most cases a significant part of the volume will not contribute to the surface,
+            // the net cost should be better or at least not worse if compared to the previous incremental version.
+            // As additional advantage, this approach simplifies the code.
             if contributions.Length > 0 then
+                let frontTopLeft = volume.Slices.[section.[0]].TopLeft + Vector3(float32 column, float32 row, 0.0f)*volume.Step
                 bestFitVertex <- frontTopLeft + (bestFitVertex /(float32 contributions.Length))
 
             {
@@ -72,8 +86,6 @@ module DualContouringIterator =
                 Position = bestFitVertex
                 Gradient = bestFitGradient
             }
-        
-        let left = volume.Slices.[frontIndex].TopLeft.X
 
         let nRows, nColumns = lastRow + 1, lastColumn + 1
 
@@ -82,27 +94,15 @@ module DualContouringIterator =
         let createDualCellsRow _ = Array.zeroCreate<DualCell> nColumns
         let innerVertices = Array.init sections.Length (fun _ -> Array.init nRows createDualCellsRow)
 
-        let mutable frontTopLeft = Vector3.Zero
-
         for n in 0..sections.Length-1 do
             let section = sections.[n]
-            let cube = Cube.create volume.Slices.[section.[0]] volume.Slices.[section.[1]] volume.IsoValue
             let mutable rowOffset = 0
 
-            frontTopLeft.Y <- volume.Slices.[section.[0]].TopLeft.Y
-            frontTopLeft.Z <- volume.Slices.[section.[0]].TopLeft.Z
-
             for row in 0..lastRow do
-
-                frontTopLeft.X <- left
-
                 for column in 0..lastColumn do
                     let tLeft = rowOffset + column
-                    innerVertices.[n].[row].[column] <- findInnerVertex frontTopLeft tLeft cube section
+                    innerVertices.[n].[row].[column] <- findInnerVertex row column tLeft section
 
-                    frontTopLeft.X <- frontTopLeft.X + volume.Step.X
-
-                frontTopLeft.Y <- frontTopLeft.Y + volume.Step.Y
                 rowOffset <- rowOffset + jumpRow
 
         for row in 0..lastRow do
